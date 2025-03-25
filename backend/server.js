cd70811acc37467880f7e1551bc20309c8c6ca62
sonu kumar <162408343+sonu8816@users.noreply.github.com>
2025-03-26 03:20:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose
 
 
   const corsOptions = {
-    origin:["http://localhost:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176"], // Replace with your frontend's origin
+    origin:["https://mid-night-fuel.vercel.app"], // Replace with your frontend's origin
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token", "admintoken" ], // Add your custom headers here
     credentials: true,
